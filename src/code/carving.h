6d14473f395fdfb921b972f0165d5b4c67a03e58
Andraž Klobučar <ak1790@hpc-login4.arnes.si>
2026-03-24 09:59:19 +0100

    Moved function definitions to carving.h

---
+++ src/code/carving.h
@@ -0,0 +1,11 @@
+#ifndef CARVING_H
+#define CARVING_H
+
+#include <stddef.h>
+
+void copy_image(unsigned char *image_out, const unsigned char *image_in, size_t size);
+void calculate_energy(unsigned char *image_out, const unsigned char *image_in, int w, int h, int cpp, int rows_per_chunk);
+void seam_carving_dynamic(const unsigned char *image_in, int w, int h, int cpp, int rows_per_chunk, int *remove_indexes);
+void remove_seams(unsigned char *image_out, const unsigned char *image_in, int in_w, int h, int cpp, int rows_per_chunk, const int *remove_indexes);
+
+#endif
